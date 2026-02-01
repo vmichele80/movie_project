8c0168d76f2765d4a8de27b578d521a9cedddeca
Mike <vmichele80@hotmail.com>
2026-02-01 12:58:24 +0100

    Added implemented file with test cases and updated the git_ignore not to upload the DB

---
+++ tests_movie_storage_functions.py
@@ -0,0 +1,17 @@
+from movie_storage_sql import add_movie, list_movies, delete_movie, update_movie
+
+# Test adding a movie
+add_movie("Inception", 2010, 8.8)
+add_movie("Wicked 2", 2025, 7.2)
+
+# Test listing movies
+movies = list_movies()
+print(movies)
+
+# Test updating a movie's rating
+# update_movie("Inception", 9.0)
+# print(list_movies())
+
+# Test deleting a movie
+# delete_movie("Inception")
+# print(list_movies())  # Should be empty if it was the only movie
