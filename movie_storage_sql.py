e36ea69d31d7c5f60220df9189ceaf34e88e0119
Mike <vmichele80@hotmail.com>
2026-02-01 13:06:15 +0100

    movie.db now created and stored within the data folder/package

--- movie_storage_sql.py
+++ movie_storage_sql.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine, text
 
 # Define the database URL
-DB_URL = "sqlite:///movies.db"
+DB_URL = "sqlite:///data/movies.db"
 
 # Create the engine
 engine = create_engine(DB_URL, echo=True)
