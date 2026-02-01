541c0e107b306a82f4e60762adfe2d0d7120c282
Mike <vmichele80@hotmail.com>
2026-02-01 12:32:28 +0100

    First commit of the movie project with the code from the first version

---
+++ movies_menu.py
@@ -0,0 +1,14 @@
+MENU = """
+********** My Movies Database **********
+
+Menu:
+0. Exit
+1. List movies
+2. Add movie
+3. Delete movie
+4. Update movie
+5. Stats
+6. Random movie
+7. Search movie
+8. Movies sorted by rating
+"""
---
+++ old_code.py
@@ -0,0 +1,218 @@
+import random
+import sys
+import movies_storage
+from movies_menu import MENU
+
+
+
+def show_menu():
+    """
+    Show the user menu saved in a separate file
+    """
+    print(MENU)
+    user_choice = input("Enter choice (0-8): ")
+    return user_choice
+
+
+def show_list_of_movies(movies):
+    """
+    Print all the movies, along with their rating.
+    In addition, the command prints how many movies there are in total in the database.
+    """
+    if len(movies) == 0:
+        print("There are no movies in the database.")
+    else:
+        print(f"{len(movies)} movies in total.")
+        for movie in movies:
+            rating = movies[movie]["rating"]
+            year = movies[movie]["year"]
+            print(f"{movie} - Rating: {rating}, Year of Release: {year}")
+
+
+def add_movie(movies):
+    """
+    Ask the user to enter a movie name, a rating and a year of release.
+    We will be using a dictionary of dictionaries where the the move title is the main key.
+    (assume that the rating is a number between 1-10).
+    """
+    movie_title = input("Enter movie name: ")
+    if movie_title not in movies:
+        movie_rating = float(input("Enter movie rating: "))
+        movie_year = int(input("Enter movie year of release: "))
+        movies[movie_title] = {
+            "rating": movie_rating,
+            "year": movie_year
+        }
+
+        movies_storage.add_movie(movie_title, movie_year, movie_rating)
+
+        print(f"""The movie {movie_title} has been added.
+            Rating {movies[movie_title]['rating']},
+            Year of release {movies[movie_title]['year']}""")
+    else:
+        print("Movie name is already in database. To update use the update option from the Menu")
+
+
+def delete_movie(movies):
+    """
+    Ask the user to enter a movie name, and delete it.
+    If the movie doesn’t exist in the database, print an error message,
+    and then print the menu again as always.
+    """
+    movie_to_delete = input("Enter movie to delete: ")
+    if movie_to_delete in movies:
+        movies_storage.delete_movie(movie_to_delete)
+        print(f"The movie {movie_to_delete} has been deleted.")
+    else:
+        print("Movie not found")
+
+
+def update_movie(movies):
+    """
+    Ask the user to enter a movie name, and then check if it exist.
+    If the movie doesn’t exist prints an error message.
+    If it exist, ask the user to enter a new rating,
+    and update the movie’s rating in the database.
+    There is no need to validate the input.
+    """
+    movie_to_update = input("Enter the movie for which you want to give an updated rating: ")
+    if movie_to_update in movies:
+        movies[movie_to_update]["rating"] = float(input("Enter new movie rating: "))
+        print(f"The rating for the movie {movie_to_update} has been updated.")
+    else:
+        print("Movie not found")
+
+
+def stats(movies):
+    """
+    give the Average rating in the Database. I have rounded to the last two decimals
+    Average
+    """
+    ratings = []
+
+    for movie in movies.values():
+        ratings.append(movie["rating"])
+
+    average = sum(ratings) / len(ratings)
+    print(f"Average rating: {average:.2f}")
+
+    # Median
+    ratings.sort()
+    num_ratings = len(ratings)
+
+    if num_ratings % 2 == 1:
+        median = ratings[num_ratings // 2]
+    else:
+        median = (ratings[num_ratings // 2 - 1] + ratings[num_ratings // 2]) / 2
+
+    print(f"Median rating: {median:.2f}")
+
+    sorted_list_of_movies = sorted(
+        movies.items(),
+        key=lambda item: item[1]["rating"],
+        reverse=True
+    )
+
+    best_rating = sorted_list_of_movies[0][1]["rating"]
+    for title, info in sorted_list_of_movies:
+        if info["rating"] == best_rating:
+            print(f"Best rated movie: {title} ({info['rating']})")
+
+    worse_rating = sorted_list_of_movies[-1][1]["rating"]
+    for title, info in sorted_list_of_movies:
+        if info["rating"] == worse_rating:
+            print(f"Worse rated movie: {title} ({info['rating']})")
+
+
+def random_movie(movies):
+    """
+    Print a random movie and its rating
+    I convert the dictionary in a list of tuples
+    I need to generate a random number and use it as an index to retrieve the item from the list
+    """
+    movies_list = list(movies.items())
+    movie_index = random.randint(0, len(movies_list) - 1)
+    print(f"""I randomly choose the following Movie for you >>
+          {movies_list[movie_index][0]} - with a rating of {movies_list[movie_index][1]['rating']} 
+          released {movies_list[movie_index][1]['year']}""")
+
+
+def search_movie(movies):
+    """
+    looks for movies already in the database
+    """
+    search_string = input("Enter part of a movie name you want to search: ").lower()
+
+    found = False
+
+    for title, info in movies.items():
+        if search_string in title.lower():
+            print(f"{title}: Rating {info['rating']}")
+            found = True
+
+    if not found:
+        print("No movies found that match your query.")
+
+
+def movies_sorted_by_rating(movies):
+    """
+    Print all the movies and their ratings,
+    in a descending order by the rating.
+    Put differently, the best movie should be printed first,
+    and the worst movie should be printed last.
+    """
+    placement = 0
+    sorted_list_of_movies  = sorted(movies.items(),
+                              key=lambda item: item[1]['rating'],
+                              reverse=True)
+    for title, info in sorted_list_of_movies:
+        placement += 1
+        print(f"Place {placement} - {title} : {info['rating']}")
+
+def press_enter_to_continue():
+    """
+    Makes the output more redeable giving the user time to read
+    """
+    input("Press Enter to continue...")
+
+def exit_app():
+    """
+    Gives the user the option to exit the app
+    """
+    print("Bye")
+    sys.exit()
+
+# Here starts the main function
+
+def main():
+    # movies_list = movies_storage.get_movies()
+
+    while True:
+        user_choice = show_menu()
+        if user_choice == "1":
+            show_list_of_movies(movies_storage.get_movies())
+        elif user_choice == "2":
+            add_movie(movies_list)
+        elif user_choice == "3":
+            delete_movie(movies_list)
+        elif user_choice == "4":
+            update_movie(movies_list)
+            movies_storage.save_movies(movies_list)
+        elif user_choice == "5":
+            stats(movies_list)
+        elif user_choice == "6":
+            random_movie(movies_list)
+        elif user_choice == "7":
+            search_movie(movies_list)
+        elif user_choice == "8":
+            movies_sorted_by_rating(movies_list)
+        elif user_choice == "0":
+            movies_storage.save_movies(movies_list)
+            exit_app()
+        else:
+            print("Invalid command")
+
+        press_enter_to_continue()
+
+if __name__ == "__main__":
+    main()
