18cd8cfff1a737cc8f511d61e5493968a273fadf
Mike <vmichele80@hotmail.com>
2026-02-01 15:54:55 +0100

    Created a py file to test omdb api. moved api key to .env and modified the .gitignore accordingly

---
+++ api_requests_test.py
@@ -0,0 +1,22 @@
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+URL_REQUEST = "http://www.omdbapi.com/?"
+API_KEY = os.getenv('API_KEY')
+
+
+movie_title = input("Please insert the title of the movie you are looking for: ").strip()
+
+print(movie_title)
+
+URL_REQUEST_MOVIE = f"{URL_REQUEST}t={movie_title}&apikey={API_KEY}"
+
+print(URL_REQUEST_MOVIE)
+
+movie_data = requests.get(URL_REQUEST_MOVIE)
+movie_data = movie_data.json()
+
+print(movie_data)
