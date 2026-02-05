76642055092388edb73e4e1994ef1d56922c4e77
Mike <vmichele80@hotmail.com>
2026-02-05 23:56:21 +0100

    changed the way the path of the db is defined to make it work on codio as well

--- movie_storage_sql.py
+++ movie_storage_sql.py
@@ -1,7 +1,12 @@
+import os
 from sqlalchemy import create_engine, text
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DB_PATH = os.path.join(BASE_DIR, "data", "movies.db")
+
 # Define the database URL
-DB_URL = "sqlite:///data/movies.db"
+DB_URL = f"sqlite:///{DB_PATH}"
+
 
 # Create the engine
 engine = create_engine(DB_URL, echo=False)
